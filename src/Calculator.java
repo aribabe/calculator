0e16fc3d663c2056493398d61eecc5652895cb7a
DESKTOP-4TARPKQ\YoungGeun <foamposite@nate.com>
2018-12-05 14:01:08 +0900

    계산기 기본 기능 구현 완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,29 +8,44 @@ public class Calculator {
 		 int first = sc.nextInt();
 		 System.out.println(first);
 		 
-		 System.out.println("연산 기호: ");
-		 String symbol= sc.next();
-		 System.out.println(symbol);
 		 
-		 System.out.println("두번째 입력 값은: ");
-		 int second = Integer.parseInt(sc.next());
-		 System.out.println(second);
-		 
-		 if(symbol.equals("+")) {
-			 System.out.println("더하기 : " + (first + second));
-		 }else if(symbol.equals("-")){
-			 System.out.println("빼기 : " + (first - second));
-		 }else if(symbol.equals("*")){
-			 System.out.println("곱하기 : " + (first * second));
-		 }else if(symbol.equals("/")){
-			 System.out.println("나누기 : " + (first / second));
-		 }else if(symbol.equals("%")){
-			 System.out.println("나머지 구하기: " + (first % second));
-		 }else {
-			 System.out.println("잘못된 값을 입력하셨습니다");
-		
+		 int result= first;
+		 while(true) {
+			 System.out.println("연산 기호: ");
+			 String symbol= sc.next();
+			 System.out.println(symbol);
+			 
+			 if(symbol.equals("quit")) {
+				 System.out.println("최종 결과 값 : " + result );
+				 break;
+			 }
+			 
+			 System.out.println("두번째 입력 값은: ");
+			 int second = Integer.parseInt(sc.next());
+			 System.out.println(second);
+			
+			 
+			 if(symbol.equals("+")) {
+				 result = result + second;
+				 System.out.println("더하기 : " + result);
+			 }else if(symbol.equals("-")){
+				 result = result - second;
+				 System.out.println("빼기 : " + result);
+			 }else if(symbol.equals("*")){
+				 result = result * second;
+				 System.out.println("곱하기 : " + result);
+			 }else if(symbol.equals("/")){
+				 result = result / second;
+				 System.out.println("나누기 : " + result);
+				 
+			 }else if(symbol.equals("%")){
+				 result = result % second;
+				 System.out.println("나머지 구하기: " + result);
+			 }else {
+				 System.out.println("잘못된 값을 입력하셨습니다");
+			
+			 }
 		 }
-		 
 		 
 //		 
 //			System.out.println("더하기 : " + (first + second));
