52ba1cd5cd62c858ad1bb9dba0619cffd03c80e1
DESKTOP-4TARPKQ\YoungGeun <foamposite@nate.com>
2018-12-05 13:52:03 +0900

    연산기호에 따라 계산

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,14 +8,35 @@ public class Calculator {
 		 int first = sc.nextInt();
 		 System.out.println(first);
 		 
+		 System.out.println("연산 기호: ");
+		 String symbol= sc.next();
+		 System.out.println(symbol);
+		 
 		 System.out.println("두번째 입력 값은: ");
 		 int second = Integer.parseInt(sc.next());
 		 System.out.println(second);
 		 
-			System.out.println("더하기 : " + (first + second));
-			System.out.println("빼기 : " + (first - second));
-			System.out.println("곱하기 : " + (first * second));
-			System.out.println("나누기 : " + (first / second));
-			System.out.println("나머지 구하기: " + (first % second));
+		 if(symbol.equals("+")) {
+			 System.out.println("더하기 : " + (first + second));
+		 }else if(symbol.equals("-")){
+			 System.out.println("빼기 : " + (first - second));
+		 }else if(symbol.equals("*")){
+			 System.out.println("곱하기 : " + (first * second));
+		 }else if(symbol.equals("/")){
+			 System.out.println("나누기 : " + (first / second));
+		 }else if(symbol.equals("%")){
+			 System.out.println("나머지 구하기: " + (first % second));
+		 }else {
+			 System.out.println("잘못된 값을 입력하셨습니다");
+		
+		 }
+		 
+		 
+//		 
+//			System.out.println("더하기 : " + (first + second));
+//			System.out.println("빼기 : " + (first - second));
+//			System.out.println("곱하기 : " + (first * second));
+//			System.out.println("나누기 : " + (first / second));
+//			System.out.println("나머지 구하기: " + (first % second));
 	}
 }
