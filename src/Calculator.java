d9040cfa5fff2efae651242ebe9151ff8aca048a
DESKTOP-4TARPKQ\YoungGeun <foamposite@nate.com>
2018-12-05 13:33:12 +0900

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+
+public class Calculator {
+	 public static void main(String[] args) {
+			System.out.println("더하기 : " + (3 + 4));
+			System.out.println("빼기 : " + (4 - 3));
+			System.out.println("곱하기 : " + (4 * 3));
+			System.out.println("나누기 : " + (4 / 2));
+			System.out.println("나머지 구하기: " + (4 % 3));
+	}
+}
