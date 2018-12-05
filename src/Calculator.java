835ad6ae5a194a8eb68f111fb73ee9bc2c99d013
DESKTOP-4TARPKQ\YoungGeun <foamposite@nate.com>
2018-12-05 13:42:56 +0900

    2개의 값을 받아 사칙연산 계산

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
 
 public class Calculator {
 	 public static void main(String[] args) {
-			System.out.println("더하기 : " + (3 + 4));
-			System.out.println("빼기 : " + (4 - 3));
-			System.out.println("곱하기 : " + (4 * 3));
-			System.out.println("나누기 : " + (4 / 2));
-			System.out.println("나머지 구하기: " + (4 % 3));
+		 
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("첫번째 입력 값은 : ");
+		 int first = sc.nextInt();
+		 System.out.println(first);
+		 
+		 System.out.println("두번째 입력 값은: ");
+		 int second = Integer.parseInt(sc.next());
+		 System.out.println(second);
+		 
+			System.out.println("더하기 : " + (first + second));
+			System.out.println("빼기 : " + (first - second));
+			System.out.println("곱하기 : " + (first * second));
+			System.out.println("나누기 : " + (first / second));
+			System.out.println("나머지 구하기: " + (first % second));
 	}
 }
