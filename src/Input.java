5cc35704cb02216906d9d0ae3dbb3fed923dab8e
DESKTOP-4TARPKQ\YoungGeun <foamposite@nate.com>
2018-12-05 14:46:46 +0900

    문자열로 값 입력받아 구현

---
+++ src/Input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Input {
+	 static int getFirstValue(Scanner scanner) {
+		 int first = scanner.nextInt();
+		return first;
+		 
+	 }
+	 
+	 static int getSecondValue(Scanner scanner) {
+		 int second = scanner.nextInt();
+		return second;
+	 }
+	 
+	 static String getSymbol(Scanner scanner) {
+		 String symbol = scanner.next();
+		return symbol;
+	 }
+}
